e9aff2c1e1c682169d3b7bc9cdc387a647414dcb
pashatya <pashatya@gmail.com>
2020-07-10 17:13:30 +0700

    sipa

---
+++ storage/logs/log-2020-06-19.php
@@ -0,0 +1,7 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2020-06-19 08:22:10 --> EA\Engine\Types\Type: Invalid argument value provided (/sipa/index.php/appointments/index/7f68d84c181b34cbf32b368a66407858)
+ERROR - 2020-06-19 08:22:10 --> #0 C:\Ampps\www\sipa\application\controllers\Appointments.php(562): EA\Engine\Types\Type->__construct('/sipa/index.php...')
+#1 C:\Ampps\www\sipa\system\core\CodeIgniter.php(532): Appointments->ajax_register_appointment()
+#2 C:\Ampps\www\sipa\index.php(353): require_once('C:\\Ampps\\www\\si...')
+#3 {main}
---
+++ storage/logs/log-2020-07-08.php
@@ -0,0 +1,14 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2020-07-08 08:22:55 --> Severity: Compile Error --> Can't use method return value in write context /var/www/html/kontak/application/controllers/Appointments.php 355
+ERROR - 2020-07-08 08:22:58 --> Severity: Compile Error --> Can't use method return value in write context /var/www/html/kontak/application/controllers/Appointments.php 355
+ERROR - 2020-07-08 16:01:44 --> Severity: Parsing Error --> syntax error, unexpected '?>' /var/www/html/kontak/application/views/backend/settings.php 530
+ERROR - 2020-07-08 16:01:47 --> Severity: Parsing Error --> syntax error, unexpected '?>' /var/www/html/kontak/application/views/backend/settings.php 530
+ERROR - 2020-07-08 16:01:48 --> Severity: Parsing Error --> syntax error, unexpected '?>' /var/www/html/kontak/application/views/backend/settings.php 530
+ERROR - 2020-07-08 16:01:49 --> Severity: Parsing Error --> syntax error, unexpected '?>' /var/www/html/kontak/application/views/backend/settings.php 530
+ERROR - 2020-07-08 16:02:00 --> Severity: Parsing Error --> syntax error, unexpected '?>' /var/www/html/kontak/application/views/backend/settings.php 530
+ERROR - 2020-07-08 16:02:04 --> Severity: Parsing Error --> syntax error, unexpected '?>' /var/www/html/kontak/application/views/backend/settings.php 530
+ERROR - 2020-07-08 16:02:24 --> Severity: Parsing Error --> syntax error, unexpected '?>' /var/www/html/kontak/application/views/backend/settings.php 530
+ERROR - 2020-07-08 16:04:17 --> Severity: Parsing Error --> syntax error, unexpected '?>' /var/www/html/kontak/application/views/backend/settings.php 519
+ERROR - 2020-07-08 16:04:21 --> Severity: Parsing Error --> syntax error, unexpected '?>' /var/www/html/kontak/application/views/backend/settings.php 519
+ERROR - 2020-07-08 16:04:22 --> Severity: Parsing Error --> syntax error, unexpected '?>' /var/www/html/kontak/application/views/backend/settings.php 519
